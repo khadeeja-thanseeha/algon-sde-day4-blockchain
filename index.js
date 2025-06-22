4e8d8f6697accdff9f23629b7c1b4aa3afc2d4af
Khadeeja Thanseeha V <117475589+khadeeja-thanseeha@users.noreply.github.com>
2025-06-22 12:40:55 +0530

    index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ myCoin.addBlock(new Block(1, Date.now().toString(), { amount: 4 }));
 console.log('⛏ Mining block 2...');
 myCoin.addBlock(new Block(2, Date.now().toString(), { amount: 8 }));
 
-myCoin.chain[1].data = { amount: 100000 };
-myCoin.chain[1].hash = myCoin.chain[1].calculateHash();
+// myCoin.chain[1].data = { amount: 100000 };
+// myCoin.chain[1].hash = myCoin.chain[1].calculateHash();
 
 console.log(JSON.stringify(myCoin, null, 2));
 console.log('Is blockchain valid?', myCoin.isChainValid());
